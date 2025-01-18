4503ed143019feff44cb7b8a7a4865e2c30a8325
Yaroshevych <aleksandr.yaroshevych@gmail.com>
2025-01-19 01:25:06 +0200

    try to make for loops easier to understand and shorter

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ def main():
     words_num = len(file_contents.split()) 
     #print(words_num)
     
-    answer = {}
-
 # it's counting unique characters and make a dic in "char":"amount" pair:
-    for i in range(0, len(file_contents)): 
-        character = file_contents[i].lower()
+    answer = {}
+    for character in file_contents:
+        character = character.lower()
         if character not in answer:
             answer[character] = 1
         else:
@@ -20,19 +19,17 @@ def main():
 
 # it's counting unique letters and make a dic in "letter":"amount" pair:
     letter_dic = {}
-    for i in range(0, len(file_contents)): 
-        character = file_contents[i].lower()
+    for character in file_contents:
+        character = character.lower()
         if character not in letter_dic and character.isalpha():
             letter_dic[character] = 1
         elif character in letter_dic and character.isalpha():
             letter_dic[character] += 1
-    #print(letter_dic)
 
 # it's converting for sorting
     newest = []
     for n in letter_dic:
         newest.append({"char":n, "num":letter_dic[n]})
-    #print(newest)
 
     def sort_on(dict):
         return dict["num"]
